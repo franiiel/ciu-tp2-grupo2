2d2a6c427df00a1947b195d527f4ce004f38bebf
Maiite18 <maitealuminep@gmail.com>
2025-10-30 15:25:58 -0300

    style inicio

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import PostCard from "../components/PostCard";
+import "./Inicio.css";
 
 
 
@@ -24,14 +25,50 @@ export default function Inicio() {
       tags: [{ id: 3, name: "paz" }],
       commentCount: 1,
     },
+    {
+      id: 3,
+      description: "Me conecté a UnaHur Anti-Social Net desde el aula y terminé hablando con nadie. 10/10 experiencia 🫠",
+      images: ["https://picsum.photos/400/200?random=3"],
+      tags: [{ id: 4, name: "unahur" }, { id: 5, name: "soledad" }],
+      commentCount: 2,
+    },
+    {
+      id: 4,
+      description: "Publicar sin esperar likes es el nuevo lujo digital ✨",
+      images: [],
+      tags: [{ id: 6, name: "reflexión" }, { id: 7, name: "minimalismo" }],
+      commentCount: 0,
+    },
+    {
+      id: 5,
+      description: "Hoy vi una publicación que decía 'no interactúes' y me sentí vista 👀",
+      images: ["https://picsum.photos/400/200?random=5"],
+      tags: [{ id: 8, name: "humor" }, { id: 9, name: "antisocial" }],
+      commentCount: 4,
+    },
+    {
+      id: 6,
+      description: "La red UnaHur Anti-Social Net me dejó publicar sin culpa. ¿Esto es libertad? 😌",
+      images: ["https://picsum.photos/400/200?random=6"],
+      tags: [{ id: 10, name: "unahur" }, { id: 11, name: "libertad" }],
+      commentCount: 1,
+    },
+    {
+      id: 7,
+      description: "Scroll, café, y cero interacciones. Mi tipo de mañana ☕",
+      images: [],
+      tags: [{ id: 12, name: "rutina" }, { id: 13, name: "antisocial" }],
+      commentCount: 3,
+    }
+
   ];
 
   return (
-    <Container className="my-4">
-      <h3 className="mb-4 text-success"> Publicaciones Recientes </h3>
-      <Row>
+    <Container className="inicio-container">
+      <h3 className="inicio-titulo">Publicaciones Recientes</h3>
+      <Row className="inicio-posts">
         {posts.map((post) => (
-          <Col md={6} lg={4}>
+          <Col key={post.id} md={6} lg={4} className="inicio-post-col">
             <PostCard {...post} />
           </Col>
         ))}
