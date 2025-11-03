4b90c68016990a06a33d8568294f10df9863d6f1
franiel <66392105+franiiel@users.noreply.github.com>
2025-11-03 13:52:27 -0300

    Usuario hardcodeado

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,6 @@
-import { Container, Card, Form, Button, Alert } from 'react-bootstrap'
-import './login.css'
-import { Link } from 'react-router-dom'
+import { Container, Card, Form, Button, Alert } from "react-bootstrap";
+import "./login.css";
+import { Link } from "react-router-dom";
 import React, { useState } from "react";
 import { useAuth } from "../components/authContext";
 
@@ -10,22 +10,33 @@ const Login: React.FC = () => {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
+  const usuarioHardcodeado = {
+    id: 1,
+    nick: "Franiel",
+    email: "franiel@example.com",
+  };
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setError("");
 
     try {
-      const res = await fetch("http://localhost:3001/users");
+      /*const res = await fetch("http://localhost:3001/users");
       const users = await res.json();
 
       // validar nick y contraseña fija
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const foundUser = users.find((u: any) => u.nickName === nickName);
-
+      
+      let foundUser = users.find(
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        (u: any) => u.nickName === nickName || usuarioHardcodeado
+      );
       if (!foundUser) {
         setError("Usuario no encontrado");
         return;
       }
+      }*/
+      const nickNameHarcoded= usuarioHardcodeado.nick
+      const foundUser = nickNameHarcoded
 
       if (password !== "123456") {
         setError("Contraseña incorrecta");
@@ -39,7 +50,7 @@ const Login: React.FC = () => {
   };
 
   return (
-     <Container className="d-flex justify-content-center align-items-center min-vh-100">
+    <Container className="d-flex justify-content-center align-items-center min-vh-100">
       <Card className="p-4 shadow-lg login-card">
         <Card.Body>
           <Card.Title className="text-center mb-4 fs-4 fw-bold text-success">
@@ -71,18 +82,17 @@ const Login: React.FC = () => {
               />
             </Form.Group>
 
-            <Button
-              variant="success"
-              type="submit"
-              className="w-100 mt-2"
-            >
+            <Button variant="success" type="submit" className="w-100 mt-2">
               Iniciar Sesión
             </Button>
           </Form>
 
           <div className="text-center mt-3">
             <span>¿No tenés cuenta? </span>
-            <Link to="/registrarse" className="text-success fw-semibold text-decoration-none">
+            <Link
+              to="/registrarse"
+              className="text-success fw-semibold text-decoration-none"
+            >
               Registrate acá
             </Link>
           </div>
