3e4ebda38d91974643525a2841a38a2e906a9b03
ethneazarate <ethneazarate@gmail.com>
2025-11-03 16:37:10 -0300

    Estilo general de la pagina

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,38 +1,43 @@
-import { Link, NavLink } from "react-router-dom";
+import type { FC } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 import "./Navbar.css";
+import logout from "../assets/iconoSalir.png";
+import logo from "../assets/logo.png";
+import refresh from "../assets/iconoRefresh.png";
 
-function NavigationBar() {
+const NavigationBar: FC = () => {
+  const navigate = useNavigate();
+
+  //logout falta hacer
+  const handleLogout = () => {
+    // lógica de cerrar sesión
+    console.log("Salir");
+    navigate("/login"); //redirigir a login
+  };
+
+  //refresh falta hacer
+  const handleRefresh = () => {
+    window.location.reload(); // recarga la página
+  };
+
   return (
-    <Navbar expand="lg" className="custom-navbar py-3 shadow-sm">
-      <Container>
-        <Navbar.Brand as={Link} to="/" className="brand-text">
+    <Navbar expand="lg" className="custom-navbar py-3 shadow-sm fixed-top">
+      <Container  className="d-flex justify-content-between align-items-center">
+        <Navbar.Brand as={Link} to="/" className="brand-text d-flex align-items-center">
+          <img src={logo} alt="Logo" className="logo-img" /> 
           UnaHur <span>Anti-Social Net</span>
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="basic-navbar-nav" className="border-0" />
-
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto nav-links">
-            <Nav.Link as={NavLink} to="/" >
-              Inicio
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/perfil">
-              Perfil
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/crearPost">
-              Publicar
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/login">
-              Iniciar Sesión
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Nav className="navbar-icons d-flex align-items-center">
+          <img src={refresh} alt="Refresh" className="nav-icon me-3" onClick={handleRefresh} style={{ cursor: "pointer" }} />
+          <img src={logout} alt="Logout" className="nav-icon" onClick={handleLogout} style={{ cursor: "pointer" }} />
+        </Nav>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavigationBar;
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,82 @@
+import { useState} from "react";
+import type { FC } from "react";
+import { NavLink } from "react-router-dom";
+import searchIcon from "../assets/iconoBuscar.png";
+import moreIcon from "../assets/iconoMas.png";
+import settingsIcon from "../assets/iconoConfig.png";
+import crearPost from "../assets/crearPublicacion.png"
+import inicio from "../assets/iconoInicio.png";
+//importar perfil o usar la foto del perfil
+import "./Sidebar.css";
+
+const Sidebar: FC = () => {
+  const [showMore, setShowMore] = useState<boolean>(false);
+
+  return (
+    <>
+      {/* Sidebar escritorio */}
+      <div className="sidebar d-none d-lg-flex flex-column justify-content-between">
+        <div>
+          <NavLink to="/" className="sidebar-link">
+            <img src={inicio} alt="Inicio" className="icon" /> Inicio
+          </NavLink>
+          <NavLink to="/buscar" className="sidebar-link">
+            <img src={searchIcon} alt="Buscar" className="icon" /> Buscar
+          </NavLink>
+          <NavLink to="/crearPost" className="sidebar-link">
+            <img src={crearPost} alt="Crear post" className="icon" /> Crear
+          </NavLink>
+           <NavLink to="/perfil" className="sidebar-link"> Perfil
+          </NavLink>
+        </div>
+
+        <div>
+          <div
+            className="sidebar-link more-link"
+            onClick={() => setShowMore(!showMore)}
+          >
+            <img src={moreIcon} alt="Más" className="icon" /> Más
+          </div>
+          {showMore && (
+            <div className="more-items">
+              <NavLink to="/configuracion" className="sidebar-link sub-item">
+                <img src={settingsIcon} alt="Configuración" className="icon" /> Configuración
+              </NavLink>
+            </div>
+          )}
+        </div>
+      </div>
+
+      {/* Barra inferior para móviles */}
+      <div className="mobile-bottom-bar d-lg-none d-flex justify-content-around">
+        <NavLink to="/" className="bottom-link">
+         <img src={inicio} alt="Inicio" className="icon" />
+        </NavLink>
+        <NavLink to="/buscar" className="bottom-link">
+          <img src={searchIcon} alt="Buscar" className="icon" />
+        </NavLink>
+        <NavLink to="/CrearPost" className="bottom-link">
+           <img src={crearPost} alt="Crear post" className="icon" />
+        </NavLink>
+        {/*aca faltaría agregar el perfil */}
+        <div>
+          <div
+            className="sidebar-link more-link"
+            onClick={() => setShowMore(!showMore)}
+          >
+            <img src={moreIcon} alt="Más" className="icon" />
+          </div>
+          {showMore && (
+            <div className="more-items">
+              <NavLink to="/configuracion" className="sidebar-link sub-item">
+                <img src={settingsIcon} alt="Configuración" className="icon" /> Configuración
+              </NavLink>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
